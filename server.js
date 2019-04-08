3cd0b51d0e352ee7baabec679e22edd9d2e298c8
WORLAND <bw0894325@otc.edu>
2019-04-08 15:19:52 -0500

    woohoo

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const people = require('./people.json');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
+});
+
+app.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person,
+    });
+  });
+  
+const server = app.listen(8500, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
+
